7d4ce673b99e423ac6cf3ee19f8f9b483ce65032
islamTaha <tahai@codescalers.com>
2016-08-08 16:34:53 +0200

    task_377 done

--- Openvcloud/Portal/end_user/admin/machines/test02_machine_creation.py
+++ Openvcloud/Portal/end_user/admin/machines/test02_machine_creation.py
@@ -1,6 +1,22 @@
+from end_user.utils import BaseTest
+from end_user.page_elements_xpath import machines_page
+import time
+import uuid
+from nose_parameterized import parameterized
+from random import randint
 
 
-class Read():
+class Read(BaseTest):
+    def __init__(self, *args, **kwargs):
+        super(Read, self).__init__(*args, **kwargs)
+        self.elements.update(machines_page.elements)
+
+    def setUp(self):
+        super(Read, self).setUp()
+        self.login()
+        if self.check_element_is_exist("left_menu") == False:
+            self.click("left_menu_button")
+        self.driver.implicitly_wait(20)
 
     def test01_machine_get(self):
         """
@@ -11,7 +27,6 @@ class Read():
         #. create new machine, should succeed
         #. get machine, should succeed
         """
-        pass
 
     def test02_machine_list(self):
         """
@@ -25,7 +40,7 @@ class Read():
         pass
 
     def test03_machine_getConsoleUrl(self):
-        """ 
+        """
         *Test case for getConsoleUrl machine.*
 
         **Test Scenario:**
@@ -58,3 +73,50 @@ class Read():
         #. getHistory of created machine, should succeed
         """
         pass
+
+
+    @parameterized.expand(["ubuntu_14_04",
+                           "ubuntu_15_10",
+                           "ubuntu_16_04",
+                           "windows_2012"
+                           ])
+    def test06_machine_create(self, image_name):
+        """ PRTL-011
+        *Test case for creating/deleting machine with all avaliable image name, random package and random disk size*
+
+        **Test Scenario:**
+
+        #. create new machine, should succeed
+        #. delete the new machine
+        :param image_name:
+        """
+        self.lg('%s STARTED' % self._testID)
+        self.click("end_user")
+        self.click("machines_button")
+        self.click("create_machine_button")
+
+        self.machine_name = str(uuid.uuid4()).replace('-', '')[0:10]
+        self.machine_description = str(uuid.uuid4()).replace('-', '')[0:10]
+        randome_package = randint(1, 6)
+        if image_name != "windows_2012":
+            random_disk_size = randint(1, 8)
+        else:
+            random_disk_size = randint(1, 6)
+            self.click("windows")
+
+        self.lg("Create a machine name: %s image:%s" % (self.machine_name, image_name))
+        self.set_text("machine_name", self.machine_name)
+        self.set_text("machine_description_", self.machine_description)
+        self.click(image_name)
+        self.click("package_%i" % randome_package)
+        self.click("disk_size_%i" % random_disk_size)
+
+        self.click("create_machine")
+        time.sleep(20)
+        self.assertEqual(self.get_text("machine_status"), "RUNNING")
+
+        self.lg("Destroy the machine")
+        self.click("destroy_machine")
+        self.click("destroy_machine_confirm")
+        time.sleep(10)
+        self.assertEqual(self.get_text("machine_list"),"Machines")
