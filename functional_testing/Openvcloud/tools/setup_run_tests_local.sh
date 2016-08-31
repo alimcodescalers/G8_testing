642224c27186390c187971f550afd1a3814f2464
Ramez Saeed <saeedr@codescalers.com>
2016-08-31 15:19:34 +0200

    run test local

--- functional_testing/Openvcloud/tools/setup_run_tests_local.sh
+++ functional_testing/Openvcloud/tools/setup_run_tests_local.sh
@@ -10,20 +10,20 @@ NC='\033[0m'       # No color
 
 mkdir -p $directory
 cd $directory
-rm -rf org_quality
+rm -rf G8_testing
 ssh-add -l
 #chmod g-r /root/.ssh/id_awesomo
-echo -e "${GREEN}** Clone org_quality $branch branch ...${NC}"
+echo -e "${GREEN}** Clone G8_testing $branch branch ...${NC}"
 ssh-add -l
 git clone -b $branch git@github.com:0-complexity/G8_testing.git
-cd G8_testing/functional_testing
+cd G8_testing
 echo -e "${GREEN}** change the environment with the correct value $environment...${NC}"
-sed -i "2s/.*/environment = $environment/" Openvcloud/config.ini
+sed -i "2s/.*/environment = $environment/" functional_testing/Openvcloud/config.ini
 echo -e "${GREEN}** Activating JumpScale virtual env ...${NC}"
 source /opt/jumpscale7/env.sh
 echo -e "${GREEN}** Checking python-pip ...${NC}";
 which pip || apt-get install -y python-pip
-echo -e "${GREEN}** Installing org_quality requirements ...${NC}"
+echo -e "${GREEN}** Installing G8_testing requirements ...${NC}"
 pip install -r requirements.txt
 echo -e "${GREEN}** Running tests ...${NC}"
 nosetests $testsuite --with-xunit --xunit-file='testresults.xml' --with-progressive --tc-file=Openvcloud/config.ini
