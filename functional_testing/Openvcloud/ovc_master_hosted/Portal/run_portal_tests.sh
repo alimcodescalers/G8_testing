cadfc3d76a570a642b2ceddcf5c41a7f8f6ad555
Mahmoud Ali <simplymahmoud@gmail.com>
2016-09-05 14:36:23 +0200

    fix portal

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/run_portal_tests.sh
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/run_portal_tests.sh
@@ -70,26 +70,10 @@ echo -e "${GREEN}** Installing portal test suite requirements ...${NC}"
 pip2 install -r requirements.txt
 echo -e "${GREEN}** Running tests ...${NC}"
 #Xvfb :99 -ac
-#export DISPLAY=:99
-echo -e "${GREEN}** Start nose for $directory browser $browser...${NC}"
-
-#if [[ -z $user_id ]]
-#then
-#    echo "read from user and passwd from config file"
-#    nosetests -v $directory --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
-#else
-#    if [[ -z $passwd ]]
-#    then
-#        echo "enter passwd for user $user_id"
-#    else
-#        echo "update user and passwd"
-#        nosetests -v $directory --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.browser:$browser  --tc=main.admin:$user_id --tc=main.admin:$passwd --with-xunit --xunit-file='testresults.xml' --with-progressive
-#    fi
-#fi
- 
-#xvfb-run -a nosetests -v $directory --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
-Xvfb :99 -ac
 export DISPLAY=:99
+
+echo -e "${GREEN}** Start nose for $directory browser $browser...${NC}"
+#xvfb-run -a nosetests -v $directory --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
 nosetests -v $directory --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
 
 # Collect result
