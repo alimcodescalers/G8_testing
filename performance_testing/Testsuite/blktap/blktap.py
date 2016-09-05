a0b21bbad3ba201438b2a93754e7af560b018ed2
john-kheir <kheirj@codescalers.com>
2016-09-05 14:49:02 +0200

    cleaning blktaps devices on cpunodes

--- performance_testing/Testsuite/blktap/blktap.py
+++ performance_testing/Testsuite/blktap/blktap.py
@@ -41,8 +41,8 @@ j.do.execute('cp Testsuite/blktap/Perf_parameters.cfg %s' %Res_dir)
 
 print('Creating %s disks on the fuse file system' %no_of_disks)
 for i in range(no_of_disks):
-    j.do.execute('qemu-img create -f raw openvstorage+tcp:10.103.1.22:26203/archive/disk%s %sG'%(i, disk_size))
-    j.do.execute("tap-ctl create -a openvstorage+tcp:10.103.1.22:26203/archive/disk%s"%i)
+    j.do.execute('qemu-img create -f raw openvstorage+tcp:10.103.1.22:26203/archive/disk%s %sG'%(i+1, disk_size))
+    j.do.execute("tap-ctl create -a openvstorage+tcp:10.103.1.22:26203/archive/disk%s"%i+1)
 
 
 #k=0
@@ -97,8 +97,5 @@ utils.collect_results(titles, results, '%s' %Res_dir)
 
 
 #cleaning
-for p in range(no_of_disks):
-    print('clean manually john')
-
-
+j.do.execute("bash Testsuite/blktap/clean")
 
