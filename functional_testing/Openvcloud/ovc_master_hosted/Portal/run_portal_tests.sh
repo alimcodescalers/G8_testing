2e9dd89fd411f8ea7f54e955f9f740033d19bd32
islamTaha <tahai@codescalers.com>
2016-09-07 14:52:00 +0200

    edit

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/run_portal_tests.sh
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/run_portal_tests.sh
@@ -61,23 +61,18 @@ echo -e "${GREEN}**  browser $browser ...${NC}"
 echo -e "${GREEN}**  branch $branch ...${NC}"
 echo -e "${GREEN}**  directory $directory ...${NC}"
 
-#cd functional_testing/Openvcloud/ovc_master_hosted/Portal
-#which pip2 || sudo apt-get install -y python-pip
-#echo -e "${GREEN}** Activating virtual env ...${NC}"
-#virtualenv venv
-#source venv/bin/activate
-#echo -e "${GREEN}** Installing portal test suite requirements ...${NC}"
-#pip2 install -r requirements.txt
-#echo -e "${GREEN}** Running tests ...${NC}"
-#Xvfb :99 -ac
-#export DISPLAY=:99
-
-#echo -e "${GREEN}** Start nose for $directory browser $browser...${NC}"
-#xvfb-run -a nosetests -v admin_portal/admin/test01_create_account_user_cs_vm.py --tc-file=config.ini --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
+cd functional_testing/Openvcloud/ovc_master_hosted/Portal
+which pip2 || sudo apt-get install -y python-pip
+echo -e "${GREEN}** Activating virtual env ...${NC}"
+virtualenv venv
+source venv/bin/activate
+echo -e "${GREEN}** Installing portal test suite requirements ...${NC}"
+pip2 install -r requirements.txt
+echo -e "${GREEN}** Running tests ...${NC}"
 
 
 sudo apt-get install xvfb
-xvfb-run -a nosetests -v $directory --tc-file=config.ini --tc=main.passwd:KrOe6gE9K5nCQdmretfXnj --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
+xvfb-run -a nosetests -v admin_portal/admin/test01_create_account_user_cs_vm.py --tc-file=config.ini --tc=main.passwd:KrOe6gE9K5nCQdmretfXnj --tc=main.env:$environment --tc=main.location:$location --tc=main.admin:$user_id --tc=main.browser:$browser  --with-xunit --xunit-file='testresults.xml' --with-progressive
 
 # Collect result
 echo -e "${GREEN}** DONE ** ...${NC}"
