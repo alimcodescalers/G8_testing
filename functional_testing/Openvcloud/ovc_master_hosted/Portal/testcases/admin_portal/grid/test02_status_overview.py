18e0447071aff9a44d4c612ca2347e1938e0d1da
islamTaha <tahai@codescalers.com>
2016-10-13 12:49:48 +0200

    PRTL 029 covers status over view from task 407

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/testcases/admin_portal/grid/test02_status_overview.py
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/testcases/admin_portal/grid/test02_status_overview.py
@@ -1,16 +1,26 @@
-# from ....utils.utils import BaseTest
-# from ...page_elements_xpath.grid import status_overview
-# 
-# 
-# class StatusTests(BaseTest):
-# 
-#     def test01_status_overview_page(self):
-#         """ PRTL-000
-#         *Test case to make sure that the process status and health check are working as expected*
-# 
-#         **Test Scenario:**
-#         #. go to status overview page
-#         #. verify machines table status/color
-#         #. press on "Run Healthcheck"
-#         #. verify expected behavior
-#         """
+from functional_testing.Openvcloud.ovc_master_hosted.Portal.framework.framework import Framework
+
+class StatusTests(Framework):
+    def setUp(self):
+        super(StatusTests, self).setUp()
+        self.Login.Login()
+        self.StatusOverview.get_it()
+
+    def test01_status_overview_page(self):
+        """ PRTL-029
+        *Test case to make sure that the process status and health check are working as expected*
+
+        **Test Scenario:**
+        #. go to status overview page
+        #. verify machines table status/color
+        #. press on "Run Healthcheck"
+        #. verify expected behavior
+        """
+        NodeStatus = self.StatusOverview.get_node_status()
+        for status in NodeStatus:
+            self.assertEqual(status, "RUNNING", "Error: %s " % status)
+
+
+
+
+
