0dfe88a316884c8c195238fc48ad468693f9dab1
john-kheir <kheirj@codescalers.com>
2016-12-05 15:35:19 +0200

    sync during mounting disks

--- performance_testing/Testsuite/1_fio_vms/mount_disks.sh
+++ performance_testing/Testsuite/1_fio_vms/mount_disks.sh
@@ -4,4 +4,4 @@ disk=$2
 echo $password | sudo -S umount /dev/vd$disk
 echo $password | sudo -S mkfs.ext4 /dev/vd$disk
 echo $password | sudo -S mkdir -p /mnt/vd$disk
-echo $password | sudo -S mount /dev/vd$disk /mnt/vd$disk
+echo $password | sudo -S mount -o sync /dev/vd$disk /mnt/vd$disk
