e80aa947191807bd0197674ee0f608de77cc25c1
dinaamagdy <magdyd@greenitglobe.com>
2017-02-08 13:18:04 +0000

    updating tear_down script

--- performance_testing/scripts/tear_down.py
+++ performance_testing/scripts/tear_down.py
@@ -3,40 +3,44 @@
 from JumpScale import j
 import ConfigParser
 import time
-from optparse import OptionParser
+from argparse import ArgumentParser
 import re
 import sys
 
-parser = OptionParser()
-parser.add_option('--clean', help='clean environment', dest='clean', default=False, action='store_true')
-(options, args) = parser.parse_args()
+
+parser = ArgumentParser()
+parser.add_argument("-u", "--users", dest="users_list", action='store', default=[],
+                    help="Provide the list of users you don't want to delete. Example: --users gig admin", nargs='+')
+parser.add_argument("-a", "--accounts", dest="accounts_list", action='store', default=[],
+                    help="Provide the list of accountss you don't want to delete. Example: --accounts gig test_deployment", nargs='+')
+parser.add_argument("-c", "--clean", dest="clean", action='store_true',
+                    help="clean all the environment", default=False)
+options = parser.parse_args()
 
 ccl = j.clients.osis.getNamespace('cloudbroker')
 pcl = j.clients.portal.getByInstance('main')
 scl = j.clients.osis.getNamespace('system')
 
-
-def delete_accounts(accounts):
+def delete_accounts(accounts, options):
     for account in accounts:
-        if account['name'] not in ['test_deployment', 'gig'] :
-            print('   |--Deleting account: %s' % account['name'])
-            pcl.actors.cloudbroker.account.delete(account['id'], reason='testing')
-            for _ in xrange(600):
-                if account['status'] == 'DESTROYED':
-                    break
-                account = ccl.account.search({'name': '%s' % account['name'], 'id': account['id']})[1]
-                time.sleep(1)
+        if account['name'] not in options.accounts_list:
+            if account['name'] not in ['test_deployment', 'gig']:
+                print('   |--Deleting account: %s' % account['name'])
+                pcl.actors.cloudbroker.account.delete(account['id'], reason='testing')
+                for _ in xrange(600):
+                    if account['status'] == 'DESTROYED':
+                        break
+                    account = ccl.account.search({'name': '%s' % account['name'], 'id': account['id']})[1]
+                    time.sleep(1)
 users_list=[]
 if options.clean:
     # if accounts with no users have been found make sure to delete them
     Nouser_accounts = ccl.account.search({'status': 'CONFIRMED', 'acl.userGroupId': ''})[1:]
     if Nouser_accounts:
-        delete_accounts(Nouser_accounts)
+        delete_accounts(Nouser_accounts, options)
     hanging_accounts = ccl.account.search({'status': 'DESTROYING'})[1:]
     if hanging_accounts:
-        delete_accounts(hanging_accounts)
-    if Nouser_accounts:
-        delete_accounts(Nouser_accounts)
+        delete_accounts(hanging_accounts, options)
     list = scl.user.list()
     for user in list:
         match = re.search('_([\S]+)', user)
@@ -52,13 +56,16 @@ else:
 print('Start tearing Down...')
 
 for user in users_list:
+    match = re.search(r'\S*DELETE\S*',user)
     user_accounts = ccl.account.search({'status': 'CONFIRMED', 'acl.userGroupId': user})[1:]
     if user_accounts:
         print('  Deleting accounts for user: %s' %user)
-        delete_accounts(user_accounts)
-    if user not in ['gig','admin']:
-        print('  Deleting user: %s' %user)
-        scl.user.delete(user)
+        delete_accounts(user_accounts, options)
+    if user not in options.users_list:
+        if user not in ['gig','admin']:
+            if not match:
+                print('  Deleting user: %s' %user)
+                scl.user.delete(user)
 
 print('Tearing Down is done')
 
