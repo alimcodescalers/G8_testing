7d06953cfe0c9d6bfee93843acc18ef68e0e92a0
islamTaha12 <tahai@codescalers.com>
2017-03-02 11:51:16 +0200

    Fix balnk pages which doesn't raise AngularException

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/framework/pages/end_user_portal/home.py
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/framework/pages/end_user_portal/home.py
@@ -5,4 +5,6 @@ class home():
     def get_it(self):
         self.framework.lg('Open end user home page')
         self.framework.get_page(self.framework.environment_url)
-        self.framework.assertTrue(self.framework.wait_until_element_located_and_has_text("end_user_home", "Machines"),"FAIL: Can't open the end user home page")
+        self.framework.assertTrue(self.framework.execute_angular_script())
+        self.framework.assertTrue(self.framework.wait_until_element_located_and_has_text("end_user_home", "Machines"),
+                                  "FAIL: Can't open the end user home page")
