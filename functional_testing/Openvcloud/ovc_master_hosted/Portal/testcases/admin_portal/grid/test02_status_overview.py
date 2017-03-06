c46c9f1b5e5b4b3e94691ae087d6f21c537e8d39
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-03-06 12:03:13 +0200

    remove _check_process_status testcase

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/testcases/admin_portal/grid/test02_status_overview.py
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/testcases/admin_portal/grid/test02_status_overview.py
@@ -6,28 +6,11 @@ class StatusTests(Framework):
         self.Login.Login()
         self.StatusOverview.get_it()
 
-    def test01_check_process_status(self):
+    def test01_health_check(self):
         """ PRTL-029
-        *Test case to make sure that the process status is working as expected*
+        *Test case to make sure that the health check is working as expected*
 
         **Test Scenario:**
-
-        #. go to status overview page
-        #. verify machines table status/color
-        #. press on "Run Healthcheck"
-        #. verify expected behavior
-        """
-        NodeStatus = self.StatusOverview.get_node_status()
-        for status in NodeStatus:
-            self.assertEqual(status, "RUNNING", "Error: %s " % status)
-
-
-    def test02_health_check(self):
-        """ PRTL-030
-        *Test case to make sure that the health check is working as expected* 
-
-        **Test Scenario:**
-
         #. go to status overview page
         #. press on "Run Healthcheck"
         #. verify expected behavior
