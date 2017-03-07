031bb25bd4adf3972f6d7a542f448d9a8e8e6d19
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-03-07 09:08:29 +0200

    fix table methods

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/framework/workflow/tables.py
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/framework/workflow/tables.py
@@ -13,7 +13,6 @@ class tables():
                      'search_box':'table_%s_search_box' % table}
         return table_dict
 
-
     def get_table_info(self, element):
         for _ in range(10):
             account_info = self.framework.get_text(element)
@@ -42,15 +41,30 @@ class tables():
         pagination = pagination.find_elements_by_tag_name('li')
         previous_button = pagination[0].find_element_by_tag_name('a')
         next_button = pagination[(len(pagination) - 1)].find_element_by_tag_name('a')
-
         return previous_button, next_button
 
-    def get_table_data(self, table):
+    def get_random_row_from_table(self, table):
+        self.framework.assertTrue(self.framework.check_element_is_exist(table['info']))
+        max_sort_value = 100
+        self.framework.select(table['selector'] , max_sort_value)
+        time.sleep(6)
+        tableData = []
+        table_rows = self.framework.get_table_rows(table['data'])
+        self.framework.assertTrue(table_rows)
+        for row in table_rows:
+            cells = row.find_elements_by_tag_name('td')
+            tableData.append([x.text for x in cells])
+        rows=len(tableData)
+        random_elemn= randint(0,rows-1)
+        random_row=tableData[random_elemn]
+        return random_row
+
+    def get_table_data(self, table, column):
         self.framework.assertTrue(self.framework.check_element_is_exist(table['info']))
         max_sort_value = 100
         account_max_number = self.get_table_max_number(table['info'])
         self.framework.select(table['selector'] , max_sort_value)
-        time.sleep(3)
+        time.sleep(6)
         page_numbers = (account_max_number / max_sort_value)
         if (account_max_number % max_sort_value) > 0:
             page_numbers += 1
@@ -61,7 +75,7 @@ class tables():
             self.framework.assertTrue(table_rows)
             for row in table_rows:
                 cells = row.find_elements_by_tag_name('td')
-                tableData.append([x.text for x in cells])
+                tableData.append(cells[column].text)
             if  page < (page_numbers-1):
                 previous_button, next_button = self.get_previous_next_button(table['pagination'])
                 next_button.click()
@@ -75,8 +89,9 @@ class tables():
 
                 text = "Showing %s to %s of %s entries" %("{:,}".format(tb_start_number), "{:,}".format(tb_end_number), "{:,}".format(tb_max_number))
                 if not self.framework.wait_until_element_located_and_has_text(table['info'], text):
-                    self.framework.lg('table max number changed %s -> %s ' % (account_max_number ,tb_max_number))
+                    self.framework.lg('table info changed %s -> %s ' % (text, self.framework.get_table_info(table['info'])))
                     return False
+
         return tableData
 
     def check_show_list(self, table):
@@ -85,7 +100,7 @@ class tables():
         rows_max_number = self.get_table_max_number(table['info'])
         for option in paging_options:
             self.framework.select(table['selector'], option)
-            time.sleep(5)
+            time.sleep(6)
             rows_max_number_ = self.get_table_max_number(table['info'])
             rows_end_number_ = self.get_table_end_number(table['info'])
             if rows_max_number != rows_max_number_:
@@ -96,7 +111,6 @@ class tables():
             else:
                 if not rows_end_number_ < option:
                     return False
-
         return True
 
     def check_next_previous_buttons(self, table):
@@ -152,7 +166,7 @@ class tables():
             self.framework.driver.execute_script("window.scrollTo(0,%d)" % (table_location['y']-50))
             element.click()
             self.framework.wait_until_element_attribute_has_text(element, 'aria-sort', 'ascending')
-            table_before = self.get_table_data(table)
+            table_before = self.get_table_data(table, column)
 
             if table_before == False:
                 return False
@@ -160,33 +174,17 @@ class tables():
             self.framework.driver.execute_script("window.scrollTo(0,%d)" % (table_location['y']-50))
             element.click()
             self.framework.wait_until_element_attribute_has_text(element, 'aria-sort', 'descending')
-            table_after = self.get_table_data(table)
+            table_after = self.get_table_data(table, column)
 
             if table_after == False:
                 return False
 
             for temp in range(len(table_before)):
-                if not table_before[temp][column] == table_after[(len(table_after)-temp-1)][column]:
+                if not table_before[temp] == table_after[(len(table_after)-temp-1)]:
                     return False
 
             self.framework.lg('coulmn %s passed' % current_column)
         return True
-
-    def get_random_row_from_table(self,table):
-        self.framework.assertTrue(self.framework.check_element_is_exist(table['info']))
-        max_sort_value = 100
-        self.framework.select(table['selector'] , max_sort_value)
-        time.sleep(3)
-        tableData = []
-        table_rows = self.framework.get_table_rows(table['data'])
-        self.framework.assertTrue(table_rows)
-        for row in table_rows:
-            cells = row.find_elements_by_tag_name('td')
-            tableData.append([x.text for x in cells])
-        rows=len(tableData)
-        random_elemn= randint(0,rows-1)
-        random_row=tableData[random_elemn]
-        return random_row
 
     def check_search_box(self, table, column_name):
         table = self.generate_table_elements(table)
@@ -197,6 +195,7 @@ class tables():
         except:
             self.framework.lg('table has not column %s' % column_name)
             return False
+
         random_row=self.get_random_row_from_table(table)
         if str(random_row[0]) == 'No data available in table':
             self.framework.lg('table is empty ')
@@ -211,16 +210,15 @@ class tables():
 
     def check_data_filters(self, table,column_name):
         table = self.generate_table_elements(table)
-        #table_before = self.get_table_data(table)
         table_head_elements = self.framework.get_table_head_elements(table['data'])
-        table_columns=[ x.text for x in  table_head_elements ]
-
+        table_columns = [ x.text for x in  table_head_elements ]
         try:
             column_index = table_columns.index(column_name)
-        except:
+        except ValueError:
             self.framework.lg('table has not column %s' % column_name)
             return False
-        random_row=self.get_random_row_from_table(table)
+
+        random_row = self.get_random_row_from_table(table)
         if str(random_row[0]) == 'No data available in table':
             self.framework.lg('no data available in table ')
             return True
