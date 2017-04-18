7942d869ee946bc910c2982cdc2c9dedb4a1fc90
islamTaha12 <tahai@codescalers.com>
2017-04-18 12:46:18 +0200

    cover test001

---
+++ functional_testing/Grid_API_Testing/api_testing/utiles/nodes_info.py
@@ -0,0 +1,14 @@
+nodes = [
+    {
+        "hostname": "",
+        "id": "dockerG8os",
+        "status": "running",
+        "ip": '10.101.110.232'
+    },
+    {
+        "hostname": "",
+        "id": "0cc47ab586fe",
+        "status": "running",
+        "ip": '147.75.100.105'
+    }
+]
