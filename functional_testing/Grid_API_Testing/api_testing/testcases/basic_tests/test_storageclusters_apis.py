65629cbd138058538f1f6ad871c7e8028066b4c6
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-04-18 18:22:33 +0200

    add storageclusters basic testcases

---
+++ functional_testing/Grid_API_Testing/api_testing/testcases/basic_tests/test_storageclusters_apis.py
@@ -0,0 +1,110 @@
+from random import randint
+from api_testing.testcases.testcases_base import TestcasesBase
+from api_testing.grid_apis.apis.storageclusters_apis import Storageclusters
+
+
+class TestStorageclustersAPI(TestcasesBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.storageclusters_api = Storageclusters()
+
+    def setUp(self):
+        super(TestStorageclustersAPI, self).setUp()
+        self.lg.info('Deploy new storage cluster (SC0)')
+        self.label = self.rand_str()
+        self.servers = randint(1,3)
+        self.types = ['nvme', 'ssd', 'hdd', 'archive']
+        self.drivetype = self.random_item(self.types)
+        self.slaveNodes = False
+        self.nodes = [self.get_random_node()]
+        self.body = {"label": self.label,
+                     "servers": self.servers,
+                     "driveType": self.drivetype,
+                     "slaveNodes": self.slaveNodes,
+                     "nodes":self.nodes}
+        self.storageclusters_api.post_storageclusters(self.body)
+
+    def tearDown(self):
+        self.lg.info('Kill storage cluster (SC0)')
+        self.storageclusters_api.delete_storageclusters_label(self.label)
+        super(TestStorageclustersAPI, self).tearDown()
+
+    def test001_get_storageclusters_label(self):
+        """ GAT-001
+        **Test Scenario:**
+        #. Deploy new storage cluster (SC0)
+        #. Get storage cluster (SC0), should succeed with 200
+        #. Get nonexisting storage cluster (SC0), should fail with 404
+        """
+        self.lg.info('Get storage cluster (SC0), should succeed with 200')
+        response = self.storageclusters_api.get_storageclusters_label(self.label)
+        self.assertEqual(response.status_code, 200)
+        for key in ['label', 'driveType', 'nodes']:
+            self.assertEqual(response.json()[key], self.body[key])
+
+        self.lg.info('Get nonexisting storage cluster (SC0), should fail with 404')
+        response = self.storageclusters_api.get_storageclusters_label('fake_label')
+        self.assertEqual(response.status_code, 404)
+
+    def test002_list_storageclusters(self):
+        """ GAT-002
+        **Test Scenario:**
+        #. Deploy new storage cluster (SC0)
+        #. List storage clusters, should succeed with 200
+        """
+        self.lg.info('Get storage cluster (SC0), should succeed with 200')
+        response = self.storageclusters_api.get_storageclusters()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.label, response.json())
+
+    def test003_deploy_new_storagecluster(self):
+        """ GAT-003
+        **Test Scenario:**
+        #. Deploy new storage cluster (SC1), should succeed with 201
+        #. List storage clusters, (SC1) should be listed
+        #. Kill storage cluster (SC0), should succeed with 204
+        """
+        self.lg.info('Deploy new storage cluster (SC1), should succeed with 201')
+        label = self.rand_str()
+        servers = randint(1,3)
+        drivetype = self.random_item(self.types)
+        slaveNodes = False
+        nodes = [self.get_random_node()]
+        body = {"label": label,
+                "servers": servers,
+                "driveType": drivetype,
+                "slaveNodes":slaveNodes,
+                "nodes":nodes}
+        self.storageclusters_api.post_storageclusters(body)
+        self.assertEqual(response.status_code, 201)
+
+        self.lg.info('List storage clusters, (SC1) should be listed')
+        response = self.storageclusters_api.get_storageclusters()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(label, response.json())
+
+        self.lg.info('Kill storage cluster (SC1), should succeed with 204')
+        response = lf.storageclusters_api.delete_storageclusters_label(label)
+        self.assertEqual(response.status_code, 204)
+
+
+    def test004_kill_storagecluster_label(self):
+        """ GAT-004
+        **Test Scenario:**
+        #. #. Deploy new storage cluster (SC0)
+        #. Kill storage cluster (SC0), should succeed with 204
+        #. List storage clusters, (SC0) should be gone
+        #. Kill nonexisting storage cluster, should fail with 404
+        """
+        self.lg.info('Kill storage cluster (SC0), should succeed with 204')
+        response = lf.storageclusters_api.delete_storageclusters_label(self.label)
+        self.assertEqual(response.status_code, 204)
+
+        self.lg.info('List storage clusters, (SC0) should be gone')
+        response = self.storageclusters_api.get_storageclusters()
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(self.label, response.json())
+
+        self.lg.info('Kill nonexisting storage cluster, should fail with 404')
+        response = lf.storageclusters_api.delete_storageclusters_label('fake_label')
+        self.assertEqual(response.status_code, 404)
