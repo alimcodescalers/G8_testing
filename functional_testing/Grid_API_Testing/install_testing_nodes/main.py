0cba0738764d24dd991631185f33a69648dfd645
islamTaha12 <tahai@codescalers.com>
2017-04-20 10:45:57 +0200

    remove docker g8os installation

--- functional_testing/Grid_API_Testing/install_testing_nodes/main.py
+++ functional_testing/Grid_API_Testing/install_testing_nodes/main.py
@@ -2,8 +2,9 @@ from termcolor import colored
 from install_testing_nodes.src.ExecuteRemoteCommands import ExecuteRemoteCommands
 
 JUMPSACLE_BRANCH = "8.2.0"
-GRID_API_BRANCH = "1.1.0-alpha"
 AYS_TEMPLATE_BRANCH = "1.1.0-alpha"
+GRID_API_BRANCH = "1.1.0-alpha"
+
 
 if __name__ == '__main__':
     executer = ExecuteRemoteCommands()
@@ -12,16 +13,16 @@ if __name__ == '__main__':
     print(colored(' [*] STEP 2 : create cloud space', 'yellow'))
     executer.create_cloudspace()
 
-    # g8os node
-    print(colored(' [*] STEP 3 : create g8os node', 'yellow'))
-    executer.create_virtualmachine()
-    executer.create_port_forward(publicPorts={22: 2200, 6379: 6379})
-    executer.connect_to_virtual_machine(port=2200)
-    executer.update_machine()
-    executer.install_docker()
-    executer.install_g8os()
-    executer.get_virtualmachine_ip()
-    executer.g8os_ip_list.append([executer.virtualmachine['ip'], 'dockerG8os'])
+    # # g8os node
+    # print(colored(' [*] STEP 3 : create g8os node', 'yellow'))
+    # executer.create_virtualmachine()
+    # executer.create_port_forward(publicPorts={22: 2200, 6379: 6379})
+    # executer.connect_to_virtual_machine(port=2200)
+    # executer.update_machine()
+    # executer.install_docker()
+    # executer.install_g8os()
+    # executer.get_virtualmachine_ip()
+    # executer.g8os_ip_list.append([executer.virtualmachine['ip'], 'dockerG8os'])
 
     # AYS server vm
     print(colored(' [*] STEP 4 : create AYS server node', 'yellow'))
@@ -29,7 +30,6 @@ if __name__ == '__main__':
     executer.create_port_forward(publicPorts={22: 2201, 5000: 5000})
     executer.connect_to_virtual_machine(port=2201)
     executer.update_machine()
-    import ipdb; ipdb.set_trace()
     executer.install_jumpscale(branch=JUMPSACLE_BRANCH)
     executer.install_g8core_python_client()
     executer.start_AYS_server()
