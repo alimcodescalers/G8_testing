b3f0c9b7f62d6693b34d80ce6dddb2bf2d62b1eb
john-kheir <kheirj@codescalers.com>
2017-04-23 09:37:52 +0200

    jobs tests

--- functional_testing/G8os/testsuite/a_basic/tests_01_system.py
+++ functional_testing/G8os/testsuite/a_basic/tests_01_system.py
@@ -637,3 +637,35 @@ class SystemTests(BaseTest):
             self.remove_container()
 
         self.lg('{} ENDED'.format(self._testID))
+
+    def test000_kill_list_jobs(self):
+
+        """ g8os-000
+        *Test case for testing killing and listing jobs*
+
+        **Test Scenario:**
+        #. Create job that runs for long time
+        #. List the job, should be found
+        #. Kill the job
+        #. List the job, shouldn't be found
+        """
+
+        self.lg('{} STARTED'.format(self._testID))
+
+        self.lg('Create job that runs for long time')
+        cmd = 'core.system'
+        match = 'sleep'
+        self.client.system('sleep 40')
+
+        self.lg('List the process, should be found')
+        id = self.get_job_id(cmd, match)
+        self.assertIsNotNone(id)
+
+        self.lg('Kill the process')
+        self.client.process.kill(id)
+
+        self.lg('List the process, shouldn\'t be found')
+        id = self.get_process_id(cmd, match)
+        self.assertIsNone(id)
+
+        self.lg('{} ENDED'.format(self._testID))
