28e03c23469e9a5eb4a045b1c9c6673b2d964fe9
john-kheir <kheirj@codescalers.com>
2017-05-10 12:42:14 +0200

    Adding kvms entended tests

--- functional_testing/G8os/testsuite/b_extended/tests_02_machines.py
+++ functional_testing/G8os/testsuite/b_extended/tests_02_machines.py
@@ -1,34 +1,106 @@
-# from utils.utils import BaseTest
-# import time
-# import unittest
+from testsuite.c_advanced.tests_03_networking import AdvancedNetworking
 # import uuid
 
-# class Machinetests(BaseTest):
-#
-#     def setUp(self):
-#         super(Machinetests, self).setUp()
-#         self.check_g8os_connection(Machinetests)
-#
-#     def test001_create_KVM_with_nbd_server(self):
-#         """ g8os-008
-#
-#         *Test case for testing creating, listing and destroying VMs*
-#
-#         **Test Scenario:**
-#
-#         #. Check that system support hardware virtualization
-#         #. Connect to nbd_server and check that image exist
-#         #. Create virtual machine (VM1) with nbd_server, should succeed
-#
-#     def test002_check_vm_after_creation(self):
-#         """ g8os-008
-#
-#         *Test case for testing main specs for vm after creation *
-#
-#         **Test Scenario:**
-#
-#         #. Check that system support hardware virtualization
-#         #. Create virtual machine (VM1) , should succeed
-#         #. ssh created vm ,should succeed
-#         #. check vm image
-#         #. check that can do basic comments through it
+class ExtendedMachines(AdvancedNetworking):
+
+    def setUp(self):
+        super(ExtendedMachine, self).setUp()
+        self.check_g8os_connection(ExtendedMachine)
+
+    def test001_kvm_add_remove_nics(self):
+        """ g8os-035
+
+        *Test case for testing adding and removing nics for vms*
+
+        **Test Scenario:**
+
+        #. Create Virtual machine (vm1)
+        #. Create vlan (v1) and specific name
+        #. create vxlan (vx1) with specific name
+        #. Create bridge with certain name
+        #. Connect the vm to all these nics types, should succeed
+        #. Deattach all these nics, should succeed
+        #. Delete (vm1)
+        """
+
+        self.lg('{} STARTED'.format(self._testID))
+
+        self.lg('Create Virtual machine (vm1)')
+        vm_name = self.rand_str()
+        self.create_vm(name=vm_name)
+
+        self.lg('Create vlan (v1) and specific name')
+        t1 = randint(1, 4094)
+        bn1 = self.rand_str()
+        elf.ovscl.json('ovs.vlan-ensure', {'master': 'backplane', 'vlan': t1, 'name': bn1})
+
+        self.lg('create vxlan (vx1) with specific name')
+        vx1_id = randint(20000, 30000)
+        vxbridge = self.rand_str()
+        self.ovscl.json('ovs.vxlan-ensure', {'master': 'vxbackend', 'vxlan': vx1_id, 'name': vxbridge})
+
+        self.lg('create bridge with certain name')
+        bn2 = self.rand_str()
+        self.client.bridge.create(bn2)
+
+        self.lg('Connect the vm to all these nics types, should succeed')
+        vm_uuid = self.get_vm_uuid(vm_name)
+        self.client.kvm.add_nic(vm_uuid, 'vlan', id=str(t1))
+        self.assertEqual(len(self.client.kvm.info(vm_uuid)['Net']), 1)
+        self.client.kvm.add_nic(vm_uuid, 'vxlan', id=str(vx1_id))
+        self.assertEqual(len(self.client.kvm.info(vm_uuid)['Net']), 2)
+        self.client.kvm.add_nic(vm_uuid, 'bridge', id=bn2)
+        self.assertEqual(len(self.client.kvm.info(vm_uuid)['Net']), 3)
+
+        self.lg('Deattach all these nics, should succeed')
+        self.client.kvm.remove_nic(vm_uuid, 'vlan', id=str(t1))
+        self.client.kvm.remove_nic(vm_uuid, 'vxlan', id=str(vx1_id))
+        self.client.kvm.remove_nic(vm_uuid, 'bridge', id=bn2)
+        self.assertEqual(len(self.client.kvm.info(vm_uuid)['Net']), 0)
+
+        self.lg('Delete (vm1)')
+        self.client.kvm.destroy(vm_uuid)
+
+        self.lg('{} ENDED'.format(self._testID))
+
+     def test001_kvm_attach_deattach_disks(self):
+         """ g8os-036
+
+         *Test case for testing attaching and deattaching disks for vms*
+
+         **Test Scenario:**
+
+         #. Create Virtual machine (vm1)
+         #. Create loop device (L1)
+         #. Attach L1 to vm1, should succeed
+         #. Attach L1 to vm1 again, vm1 should still see L1 as one vdisk
+         #. Deattach L1 from vm1, should succeed
+         #. Delete (vm1)
+         """
+
+         self.lg('{} STARTED'.format(self._testID))
+         self.lg('Create Virtual machine (vm1)')
+         vm_name = self.rand_str()
+         self.create_vm(name=vm_name)
+
+         self.lg('Create loop device (L1)')
+         loop_dev = self.setup_loop_devices(['bd0'], '500M', deattach=True)[0]
+
+         self.lg('Attach L1 to vm1, should succeed')
+         l = len(self.client.kvm.info(vm_uuid)['Block'])
+         vm_uuid = self.get_vm_uuid(vm_name)
+         self.client.kvm.attach_disk(vm_uuid, {'url': loop_dev})
+         self.assertEqual(len(self.client.kvm.info(vm_uuid)['Block']), l+1)
+
+         self.lg('Attach L1 to vm1 again, vm1 should still see L1 as one vdisk')
+         self.client.kvm.attach_disk(vm_uuid, {'url': loop_dev})
+         self.assertEqual(len(self.client.kvm.info(vm_uuid)['Block']), l+1)
+
+         self.lg('Deattach L1 from vm1, should succeed')
+         self.client.kvm.detach_disk(vm_uuid, {'url': loop_dev})
+         self.assertEqual(len(self.client.kvm.info(vm_uuid)['Block']), l)
+
+         self.lg('Delete (vm1)')
+         self.client.kvm.destroy(vm_uuid)
+
+         self.lg('{} ENDED'.format(self._testID))
