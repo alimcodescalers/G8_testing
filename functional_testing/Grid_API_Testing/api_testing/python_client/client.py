9d619b24a2b3b55541543c1fc0a672f679606e72
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2017-05-16 11:24:17 +0200

    extend containers testcases

--- functional_testing/Grid_API_Testing/api_testing/python_client/client.py
+++ functional_testing/Grid_API_Testing/api_testing/python_client/client.py
@@ -157,8 +157,19 @@ class Client:
         processes = self.client.process.list()
         return processes
 
+    def get_container_client(self,container_name):
+        container = self.client.container.find(container_name)
+        if not container:
+            return False
+        container_id = list(container.keys())[0]
+        container_client = self.client.container.client(int(container_id))
+        return container_client
+
     def get_container_info(self, container_id):
-        container_id = list(self.client.container.find(container_id).keys())[0]
+        container=self.client.container.find(container_id)
+        if not container:
+            return False
+        container_id = list(container.keys())[0]
         container_info = {}
         golden_data = self.client.container.list().get(str(container_id), None)
         if not golden_data:
