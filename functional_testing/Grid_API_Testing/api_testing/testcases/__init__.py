1e4a25186baac14fe370e1780666cf21a854b4d6
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-05-21 09:15:13 +0200

    fix

--- functional_testing/Grid_API_Testing/api_testing/testcases/__init__.py
+++ functional_testing/Grid_API_Testing/api_testing/testcases/__init__.py
@@ -1,7 +1,9 @@
 from api_testing.grid_apis.apis.nodes_apis import NodesAPI
+from unittest import TestCase
 
 def get_node_physical_ip(nodeid):
     response = nodes_api.get_nodes_nodeid_nics(nodeid)
+    tc.assertEqual(response.status_code, 200)
     for nic in response.json():
         nic_hwaddr = nic['hardwareaddr'].replace(':', '') 
         if nic_hwaddr == nodeid:
@@ -12,6 +14,7 @@ def get_node_info():
     nodes_api = NodesAPI()
     nodes_info = []
     response = nodes_api.get_nodes()
+    tc.assertEqual(response.status_code, 200)
     for node in response.json():
         ip = get_node_physical_ip(node['id'])
         nodes_info.append({"id":node['id'],
@@ -19,5 +22,6 @@ def get_node_info():
                             "status":node['status']})
     return nodes_info
 
+tc = TestCase()
 nodes_api = NodesAPI()
 NODES_INFO = get_node_info()
